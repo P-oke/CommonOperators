5d14d8e1a05b2470500502479b0d5deb024194fc
Oke Paul <paulayomikun@gmail.com>
2021-04-20 10:14:59 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CommonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +, *, /, _, %, >, < >=, <=
+            int x = 20;
+            int y = 10;
+
+            string result = x > y ? " X is greater than Y" : " X is less than Y";
+
+            Console.WriteLine(result);
+
+            int val = 6;
+            // val++;
+            val--;
+            // val += 2;
+            Console.WriteLine(val);
+
+
+
+
+
+        }
+    }
+}
